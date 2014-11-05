90880f1651fa667243d5da41a1e8395d2bb110af
Aaron Bieber <deftly@gmail.com>
2014-11-05 06:31:30 -0700

    committed fritzing

--- cad/fritzing/files/fritzing.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# $OpenBSD: fritzing.sh,v 1.1.1.1 2010/09/15 07:14:18 jasper Exp $
-# Since Fritzing wants it's file stored in CWD or specified via --folder
-# we use the last. Lest to prevent ever changing patches.
-
-${TRUEPREFIX}/bin/Fritzing --folder ${TRUEPREFIX}/share/fritzing/ "$@"
